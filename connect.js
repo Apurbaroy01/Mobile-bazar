2958ebe6eac6b376546213bfb90803183732a40f
apurba-00011 <apurbaroy00011@gmail.com>
2025-05-22 21:41:16 +0600

    8th

--- connect.js
+++ connect.js
@@ -11,3 +11,30 @@ function goBack() {
 }
 
 
+const track = document.getElementById('carouselTrack');
+  const carousel = document.getElementById('smoothCarousel');
+  const slideWidth = carousel.offsetWidth;
+  let index = 0;
+
+  setInterval(() => {
+    index++;
+    if (index >= track.children.length) {
+      index = 0;
+    }
+    track.style.transform = `translateX(-${slideWidth * index}px)`;
+  }, 3000);
+
+
+const tracks = document.getElementById('carouselTracks');
+  const carousels = document.getElementById('smoothCarousels');
+  const slideWidths = carousels.offsetWidth;
+  let indexs = 0;
+
+  setInterval(() => {
+    indexs++;
+    if (indexs >= tracks.children.length) {
+      indexs = 0;
+    }
+    tracks.style.transform = `translateX(-${slideWidths * indexs}px)`;
+  }, 3000);
+  
